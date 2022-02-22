2a85fc63c671f4ab30703da2ee046b7f51b472f7
whz <2660405721@qq.com>
2022-02-22 23:26:37 +0800

    add 0704 二分法最简单的题

---
+++ 0704_search.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: [int], target: int) -> int:
+        l=0
+        r=len(nums)-1
+        while l<r:
+            mid=(l+r)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                l=mid+1
+            else:
+                r=mid-1
+        return -1
+
+
+if __name__ == '__main__':
+            s = Solution()
+            print(s.search())
