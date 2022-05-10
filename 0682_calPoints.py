03d4a88abb06343b2cc2cd22ae23b77b671f6a77
whz <2660405721@qq.com>
2022-05-10 22:54:29 +0800

    add 0682 栈

---
+++ 0682_calPoints.py
@@ -0,0 +1,22 @@
+class Solution:
+    def calPoints(self, ops: [str]) -> int:
+        ans = 0
+        points = []
+        for op in ops:
+            if op == '+':
+                pt = points[-1] + points[-2]
+            elif op == 'D':
+                pt = points[-1] * 2
+            elif op == 'C':
+                ans -= points.pop()
+                continue
+            else:
+                pt = int(op)
+            ans += pt
+            points.append(pt)
+        return ans
+
+
+if __name__ == '__main__':
+            s = Solution()
+            print(s.calPoints(["5","2","C","D","+"]))
