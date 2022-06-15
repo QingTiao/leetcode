6057baa5ea35963275dcd1240008c9f682480db8
whz <2660405721@qq.com>
2022-06-15 15:40:31 +0800

    add 1646 动态规划 模拟 简单

---
+++ 1646_getMaximumGenerated.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        if n == 0:
+            return 0
+        nums = [0] * (n + 1)
+        nums[1] = 1
+        for i in range(2, n + 1):
+            if i%2==0:
+                nums[i]=nums[i//2]
+            else:
+                nums[i]=nums[(i-1)//2]+nums[(i+1)//2]
+        return max(nums)
+
+
+
+
+if __name__ == '__main__':
+            s = Solution()
+            print(s.getMaximumGenerated(7))
